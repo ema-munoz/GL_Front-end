aadfb610b5037afa30e69c99287997873ff8b6a4
Jenny Sinchiguano <67289650+jlm-sinchiguano@users.noreply.github.com>
2022-08-18 11:58:48 -0500

    Teacher create

--- src/app/teacher/create-teachers/create-teachers.component.ts
+++ src/app/teacher/create-teachers/create-teachers.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TeachersModel } from 'src/app/models/teacher.model';
+import { TeacherHttpService } from 'src/app/services/teacher.service';
 
 @Component({
   selector: 'app-create-teachers',
@@ -7,9 +11,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateTeachersComponent implements OnInit {
 
-  constructor() { }
+  careers: TeachersModel[] = [];
+  title: string = 'Nuevo Docente';
+  myForm: FormGroup;
+
+  constructor(
+    private teacherHttpService: TeacherHttpService,
+    private router: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    private route:Router,
+  ) { 
+    this.myForm = this.newForm();
+  }
 
   ngOnInit(): void {
   }
 
+  newForm(): FormGroup {
+
+    return this.formBuilder.group({
+      identificationCard:[null, [Validators.required, Validators.minLength(2), Validators.maxLength(11)]],
+      names: [null, [Validators.required, Validators.minLength(2)]],
+      surnames: [null, [Validators.required, Validators.minLength(2)]],
+      institutionalEmail: [null, [ Validators.email]],
+      phone: [null, [ Validators.minLength(2), Validators.maxLength(20)]],
+    })
+  }
+
+  store() {
+
+    this.teacherHttpService.store(this.myForm.value).subscribe(
+      response => {
+        this.route.navigate(['list-teachers']);
+      }
+    );
+  }
+
+  get idField() {
+    return this.myForm.controls['id'];
+  }
+
+  get identificationCardField() {
+    return this.myForm.controls['identificationCard'];
+  }
+
+  get namesField() {
+    return this.myForm.controls['names'];
+  }
+
+  get surnamesField() {
+    return this.myForm.controls['surnames'];
+  }
+
+  get institutionalEmailField() {
+    return this.myForm.controls['institutionalEmail'];
+  }
+
+  get phoneField() {
+    return this.myForm.controls['phoneEmail'];
+  }
+
 }
+
